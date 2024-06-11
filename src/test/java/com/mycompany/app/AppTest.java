5a8efb34329e319f54dadf5f425df299cd66f008
papebily <papebily93@gmail.com>
2024-06-11 07:22:31 -0400

    ajout de quelques tests

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -17,4 +17,16 @@ public class AppTest
     {
         assertTrue( true );
     }
+   @Test
+   public void shouldAnswerWithTrue1()
+   {
+      assertTrue(true);
+   }
+  @Test 
+    public void shouldAnswerWithTrue2()
+   {
+      assertTrue(true);
+   }
+
+
 }
